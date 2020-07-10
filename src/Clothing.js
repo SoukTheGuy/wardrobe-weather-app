f9ccd3fb8f11acddc91018fee718393c2c1806af
SoukTheGuy <64669177+SoukTheGuy@users.noreply.github.com>
2020-07-10 08:36:45 -0500

    Create Clothing.js

---
+++ src/Clothing.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Clothing = () => {
+    return (
+        <div>
+  <table>
+  <tr>
+    <th>Recommended Summer Clothing</th>
+  </tr>
+  <tr>Tank Top</tr>
+  <tr>T-Shirt</tr>
+  <tr>Shorts</tr>
+  <tr>Sandals</tr>
+</table>
+<table>
+  <tr>
+    <th>Recommended Fall Clothing</th>
+  </tr>
+  <tr>Long Sleeve Shirt</tr>
+  <tr>T-Shirt</tr>
+  <tr>Pants</tr>
+  <tr>Low Socks</tr>
+  <tr>Shoes</tr>
+</table>
+<table>
+  <tr>
+    <th>Recommended Winter Clothing</th>
+  </tr>
+  <tr>Long Sleeve Shirt</tr>
+  <tr>Jacket</tr>
+  <tr>Pants</tr>
+  <tr>High Socks</tr>
+  <tr>Boots</tr>
+</table>
+<table>
+  <tr>
+    <th>Recommended Spring Clothing</th>
+  </tr>
+  <tr>Tank Top</tr>
+  <tr>T-Shirt</tr>
+  <tr>Shorts</tr>
+  <tr>Sandals</tr>
+  <tr>Shoes</tr>
+  <tr>Low Socks</tr>
+</table>
+        </div>
+    )
+}
+
+export default Clothing;
